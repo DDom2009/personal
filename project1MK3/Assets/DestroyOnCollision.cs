248db5cc1df38ca4a3946897942f0efffa67cbca
ddom2015 <guez.domin@gmail.com>
2021-05-12 01:32:49 -0400

    project1
    project 1 late

---
+++ project1MK3/Assets/DestroyOnCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DestroyOnCollision : MonoBehaviour
+{
+
+
+
+
+	//here we will destroy the game object the script is attached to if it collides with the player, as long as the tag "Player" has been assigned to the player.
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+			Destroy(gameObject);
+
+	}
+}
